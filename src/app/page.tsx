6eeecd2182d39bca7c62437ee9ee67a4342ba50b
Ali Raza <129832771+Raza118041@users.noreply.github.com>
2025-05-07 22:23:22 +0500

    make cta component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import BeautyGuide from "./components/BeautyGuide/beautyguide";
-import CTA from "./components/CTA Section/cta";
+import CTA from "./components/CTASection/cta";
 import HeroSection from "./components/HeroSection/herosection";
 import Service from "./components/Services/service";
 import Transformation from "./components/Transformation/transformation";
