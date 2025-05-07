8541b1e3ed138eb04390de081b7d57c71b163018
Ali Raza <129832771+Raza118041@users.noreply.github.com>
2025-05-07 15:39:51 +0500

    Update herosection.tsx

--- src/app/components/heroSection/herosection.tsx
+++ src/app/components/heroSection/herosection.tsx
@@ -1,10 +1,9 @@
 import Image from 'next/image'
 import React from 'react'
-import women from "../../../../public/Images/girl.jpg"
-import vector from "../../../../public/Images/Vector.svg"
 import phone from "../../../../public/Images/phone-1.svg"
 import { Button } from '@/components/ui/button'
 import { H1, P } from '@/components/ui/Typography'
+import Link from 'next/link'
 
 const HeroSection = () => {
     return (
@@ -20,30 +19,24 @@ const HeroSection = () => {
             </video>
             <div className="relative z-10 flex justify-between h-full">
                 <div className="flex flex-col justify-center p-20 text-white bg-black/20 w-full backdrop-blur-sm">
-                    {/* <div>
-                        <Image
-                            src={vector}
-                            alt="logo"
-                            width={30}
-                            height={30}
-                            className="w-32 object-cover mb-10"
-                        />
-                    </div> */}
                     <H1 className='text-[#FF835A]/80 mb-3 text-4xl'>Glow naturally with skin care</H1>
                     <P className="w-full">Our skincare line is crafted with pure,</P>
                     <P className="mb-6">high-quality ingredients for visible results.</P>
                     <div className="w-auto">
-                        <Button
-                            className="px-8 bg-[#FF835A] text-[#512729] py-6 rounded-none rounded-tr-xl hover:bg-[#FF835A] cursor-pointer flex items-center gap-2"
-                        >
-                            <Image
-                                src={phone}
-                                alt="phone"
-                                width={20}
-                                height={20}
-                            />
-                            Book an appointment
-                        </Button>
+                        <Link href={"https://calendly.com/"} target='_blank'>
+                            <Button
+                                className="px-8 group bg-[#FF835A] text-[#512729] py-6 rounded-none rounded-tr-xl hover:bg-[#FF835A] hover:text-white cursor-pointer flex items-center gap-2"
+                            >
+                                <Image
+                                    src={phone}
+                                    alt="phone"
+                                    width={20}
+                                    height={20}
+                                    className='transition-all duration-300 group-hover:invert group-hover:brightness-0 group-hover:contrast-200'
+                                />
+                                Book an appointment
+                            </Button>
+                        </Link>
                     </div>
                 </div>
             </div>
