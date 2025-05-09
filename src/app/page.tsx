c542301969eeac37582784883a194ed4759e4796
Ali Raza <129832771+Raza118041@users.noreply.github.com>
2025-05-09 14:13:59 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import Articles from "@/app/components/ArticlesSection/articles";
-import BeautyGuide from "@/app/components/BeautyGuide/beautyguide";
-import CTA from "@/app/components/CTASection/cta";
-import Gallery from "@/app/components/GallerySection/gallery";
-import HeroSection from "@/app/components/HeroSection/herosection";
-import Sponsors from "@/app/components/OurSponsors/Sponsors";
-import Service from "@/app/components/Services/service";
-import Statistics from "@/app/components/StatisticsSection/Statistics";
-import Testimonial from "@/app/components/Testimonial/Testimonial";
-import Transformation from "@/app/components/Transformation/transformation";
+import Articles from "../app/components/ArticlesSection/articles";
+import BeautyGuide from "../app/components/BeautyGuide/beautyguide";
+import CTA from "../app/components/CTASection/cta";
+import Gallery from "../app/components/GallerySection/gallery";
+import HeroSection from "../app/components/HeroSection/herosection";
+import Sponsors from "../app/components/OurSponsors/Sponsors";
+import Service from "../app/components/Services/service";
+import Statistics from "../app/components/StatisticsSection/Statistics";
+import Testimonial from "../app/components/Testimonial/Testimonial";
+import Transformation from "../app/components/Transformation/transformation";
 
 export default function Home() {
   return (
